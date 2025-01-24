f1bfefe46eddf04f4098839927d5a28a20a49bb6
Felipe Fernando Gonçalves <felipefernando94@gmail.com>
2025-01-24 11:46:50 -0300

    first commit

---
+++ src/middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/integrations/:path*",
+};
+
+export function middleware(request) {
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-createxyz-project-id", "b62aa8c1-bbff-4c11-b67f-5f75a34eb0ea");
+
+  request.nextUrl.href = `https://www.create.xyz/${request.nextUrl.pathname}`;
+
+  return NextResponse.rewrite(request.nextUrl, {
+    request: {
+      headers: requestHeaders,
+    },
+  });
+}
